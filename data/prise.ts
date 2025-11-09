6b7e890131e186dacc3c457e0af69c8985c7ce1f
roxybai0101-byte <roxybai0101@gmail.com>
2025-11-09 09:16:48 +0300

    Create prise.ts

---
+++ data/prise.ts
@@ -0,0 +1,6 @@
+// data/prices.ts
+export const PRICES = [
+  { title: "Пудровые брови", price: "12 000 ₽", note: "Коррекция 6 000 ₽" },
+  { title: "Губы (акварель)", price: "15 000 ₽" },
+  { title: "Межресничка", price: "10 000 ₽" },
+];
